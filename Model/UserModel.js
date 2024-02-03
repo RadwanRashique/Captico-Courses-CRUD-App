341eec794fc786d2a24da4a76f239d11a034496e
Radwan Rashique P <radwangoinspire916@gmail.com>
2024-02-03 12:10:18 +0530

    schema setup for usreData done

---
+++ Model/UserModel.js
@@ -0,0 +1,21 @@
+const mongoose=require('mongoose')
+const userSchema=mongoose.Schema({
+    name:{
+        type:String
+    },
+    email:{
+        type:String
+    },
+    phone:{
+        type:String
+    },
+    password:{
+        type:String
+    }
+
+},{
+timestamps:true
+})
+
+const UserModel=mongoose.model("userDetails",userSchema) 
+module.exports=UserModel

741a6c4fcc571e6e44832299e051579404d8c30a
Radwan Rashique P <radwangoinspire916@gmail.com>
2024-02-03 12:20:10 +0530

    course model setup done

---
+++ Model/CourseModel.js
@@ -0,0 +1,20 @@
+const mongoose=require('mongoose')
+const courseSchema=mongoose.Schema({
+    courseName:{
+        type:String
+    },
+    courseImage:{
+        type:String
+    },
+    courseDescription:{
+        type:String
+    },
+    price:{
+        type:Number
+    }
+},{
+    timestamps:true
+})
+
+const courseModel=mongoose.model('courseDetails',courseSchema)
+module.exports= courseModel

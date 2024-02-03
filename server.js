70bf555fcaef144dad3cb6f45398c2a30d67c2cc
Radwan Rashique P <radwangoinspire916@gmail.com>
2024-02-03 11:39:00 +0530

    backend complete file and folder structure done

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express=require('express')
 const mongoose=require('mongoose')
 const cors=require('cors')
 const dotenv=require('dotenv')
+dotenv.config()
 const app=express()
 app.use(cors("*"))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 
-app.use('/api/user',UserRoute)
+// app.use('/api/user',UserRoute)
 const port = process.env.PORT
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)

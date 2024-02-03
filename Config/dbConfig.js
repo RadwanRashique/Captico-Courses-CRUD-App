b01c9b03cd0b398634dd374e617a215ba8079913
Radwan Rashique P <radwangoinspire916@gmail.com>
2024-02-03 11:48:36 +0530

    database connection setup done

---
+++ Config/dbConfig.js
@@ -0,0 +1,18 @@
+const mongoose=require('mongoose')
+const dotenv=require('dotenv').config()
+
+const MONGO_URL=process.env.MONGODB_URL
+
+const dbConfig=()=>{
+    try{
+        mongoose.connect(MONGO_URL)
+        console.log("MongoDB database connected")
+
+    }
+    catch(error){
+        console.error(error)
+        process.exit(1)
+    }
+}
+
+module.exports=dbConfig
--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express=require('express')
 const mongoose=require('mongoose')
 const cors=require('cors')
 const dotenv=require('dotenv')
+
 dotenv.config()
+const dbConnection=require('./Config/dbConfig')
+
 const app=express()
 app.use(cors("*"))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-
+dbConnection()
 // app.use('/api/user',UserRoute)
 const port = process.env.PORT
 app.listen(port,()=>{

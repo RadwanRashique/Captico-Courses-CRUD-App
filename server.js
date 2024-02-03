c16c2aeb4c22283e7f593d17a1ac02e0143ed295
Radwan Rashique P <radwangoinspire916@gmail.com>
2024-02-03 11:26:01 +0530

    server setup done

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const cors=require('cors')
+const dotenv=require('dotenv')
+const app=express()
+app.use(cors("*"))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
+
+app.use('/api/user',UserRoute)
+const port = process.env.PORT
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})

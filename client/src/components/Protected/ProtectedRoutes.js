024844c706a864611f3da4767f79e67af88ef3e7
Radwan Rashique P <radwangoinspire916@gmail.com>
2024-02-05 07:34:17 +0530

    some internal checks done

--- client/src/components/Protected/ProtectedRoutes.js
+++ client/src/components/Protected/ProtectedRoutes.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 import { RouteVariables } from '../../util/RouteVariables'
 function UserProtectedRoute(props) {
-  // so if there is no token it will navigate to the login page
+  
   if (localStorage.getItem('token')) {
     console.log(localStorage.getItem('token',"888"))
     return props.children
